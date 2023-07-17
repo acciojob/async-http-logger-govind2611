b626147c118b9efa06829b63a30f72df08b09478
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-17 09:44:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const axios = require('axios');
+
+async function makeRequest() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+makeRequest();
